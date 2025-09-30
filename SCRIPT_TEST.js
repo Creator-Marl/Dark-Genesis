a81be4012d099703ecfc05bed02569ddb33ffa63
Creator-Marl <marlloisir8@gmail.com>
2025-09-30 15:11:07 +0200

    Update SCRIPT_TEST.js

--- SCRIPT_TEST.js
+++ SCRIPT_TEST.js
@@ -15,7 +15,7 @@ function openTab(event, tabName) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var loginForm = document.querySelector('#Tab2 and #Tab1 form');
+    var loginForm = document.querySelector('#Tab1 form');
     loginForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Empêche le comportement par défaut du formulaire
 
@@ -45,6 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
 
 
 
